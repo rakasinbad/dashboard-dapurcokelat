38df0ddd6cebdb5d629b2567de1c2f728f1e3e2d
Reza Raka Nugraha <rezaraka00@gmail.com>
2025-12-31 10:32:35 +0700

    add query insert compliment

---
+++ compliment/insert_bulk_compliment.sql
@@ -0,0 +1,158 @@
+so base on the query i sent here are the bulk data, adjust the row order for each data, for waktu and every data that have same value, means it will be same for all row, for nocomp use outlet code for each row, in the behind of text
+
+nocomp: CP20251231094349${outlet}
+waktu: 2025-12-31 09:43:49
+nik: 140102355
+dari: Marketing
+untuk: use all outlet
+keterangan: Walkin (TikTok GO)
+qty: 1
+price: 0
+status: 1
+
+outlet, (used for outlet and untuk):
+GF
+CI
+RK
+JS 
+GO
+TS
+TS
+MD
+RN
+TU
+CK
+BR
+TM
+ML
+ML
+MJ
+HI 
+TS
+DK
+CP 
+KC
+BO 
+HI 
+BO 
+JG 
+BI 
+AS
+AS
+AS 
+GV 
+TS
+TS 
+TL 
+TL 
+GO 
+CK 
+BR 
+GY 
+AS
+TS 
+TS 
+JS 
+BL 
+MN 
+CI 
+SMG 
+CI
+
+kode:
+01080600
+01040100
+01480600 
+01480600 
+01480600
+01480600
+01080600 
+01480600
+01480600
+01480600
+01480600
+01480600
+01480600
+01430600
+01480600
+01430600
+01430600
+01480600
+01480600
+01480600
+01430600
+01430600
+01480600
+01480600
+01480600
+01480600
+01430600
+01480600
+01430600
+01430600
+01480600
+01480600
+01430600
+01430600
+01480600
+01430600
+01430600
+01480600
+01430600
+01430600
+01430600
+01480600
+01430600
+01430600
+01430600
+01480600
+01430600
+
+nama: 
+Two Seasons Cake Loaf
+Opera Cake Reg
+Chocolava Cake Loaf 
+Chocolava Cake Loaf 
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+Two Seasons Loaf 
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+BLACK FOREST LOAF
+Chocolava Cake Loaf
+BLACK FOREST LOAF
+BLACK FOREST LOAF
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+BLACK FOREST LOAF
+BLACK FOREST LOAF
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+BLACK FOREST LOAF
+Chocolava Cake Loaf
+BLACK FOREST LOAF
+BLACK FOREST LOAF
+Chocolava Cake Loaf
+Chocolava Cake Loaf
+BLACK FOREST LOAF
+BLACK FOREST LOAF
+Chocolava Cake Loaf
+BLACK FOREST LOAF
+Black forest loaf
+Chocolava Cake Loaf
+Black forest loaf
+Black forest loaf
+Black forest loaf
+Chocolava Cake Loaf
+Black forest loaf
+Black forest loaf
+Black forest loaf
+Chocolava Cake Loaf
+Black forest loaf
