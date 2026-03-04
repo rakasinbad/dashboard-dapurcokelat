87000f002c3bc8c96ea963fcdc41b5ee196a74b9
Reza Raka Nugraha <rezaraka00@gmail.com>
2026-03-04 15:43:44 +0700

    refactor(metabase): reorganize SQL queries into sales data subdirectories
    - Move daily sales SQL files to dedicated 'sales data daily' folder
    - Add new 'chart.bak.sql' for franchise sales analysis
    - Improve organization of metabase query files by category

---
+++ metabase/sales data franchise only/list-data.sql
@@ -0,0 +1,168 @@
+select
+  tgl_trx as "Tanggal Trx",
+  outlet as "Outlet",
+  sum(bruto) as "Total Gross Sales",
+  sum(diskon_rupiah) as "Total Discount",
+  sum(netto) as "Total Nett Sales",
+  count(storage_transaksi.no_trx) as "Number of Orders",
+  channel as "Channel"
+from
+  (
+    select
+      tgl_trx,
+      outlet,
+      bruto,
+      diskon_rupiah,
+      netto,
+      no_trx,
+      CASE
+        WHEN coalesce(stc.channel, src.channel) IN ('Web', 'App') THEN 'Web + App'
+        WHEN coalesce(stc.channel, src.channel) IN ('GOFOOD', 'GRABFOOD', 'SHOPEE FOOD')
+        AND src.channel != 'Walk In' THEN 'Food Delivery'
+        WHEN coalesce(stc.channel, src.channel) IN (
+          'ECOMMERCE - Tokopedia',
+          'ECOMMERCE - SHOPEE MALL',
+          'Tik Tok'
+        ) THEN 'E-Commerce'
+        WHEN coalesce(src.channel = 'Walk In') THEN 'Walk In'
+        ELSE coalesce(src.channel, src.channel)
+      END as channel
+    from
+      (
+        select
+          storage_transaksi.id,
+          6 as kodedb_id,
+          storage_transaksi.outlet,
+          storage_transaksi.tgl_trx,
+          storage_transaksi.no_trx,
+          storage_transaksi.namapemesan,
+          storage_transaksi.tlptujuan,
+          storage_transaksi.bruto,
+          storage_transaksi.diskon_rupiah,
+          storage_transaksi.netto,
+          storage_transaksi.remark,
+          outlet.kode,
+          outlet.panjang,
+          tempo.customer.nama,
+          tempo.`customer`.`alamat`,
+          tempo.`customer`.`phone`,
+          tempo.`customer`.`customer_gender`,
+          tempo.`customer`.`customer_dob`,
+          { { snippet: Channel Dictionary } }
+        from
+          dci.storage_transaksi
+          JOIN dci.outlet on storage_transaksi.outlet = outlet.kode
+          LEFT JOIN tempo.`customer` ON `storage_transaksi`.`nomember_app` = tempo.`customer`.`nomember`
+          AND `storage_transaksi`.`nomember` = tempo.`customer`.`id`
+        where
+          storage_transaksi.spoil = 0
+          AND storage_transaksi.compliment = 0
+          AND (
+            `storage_transaksi`.`outlet` != 'XC'
+            OR `storage_transaksi`.`outlet` != 'XT'
+            OR `storage_transaksi`.`outlet` != 'XX'
+          ) [[AND {{periode}}]]
+		and jenistrx in ('1','4','8')
+		and storage_transaksi.`status` not in ('0', '')
+	/*	and storage_transaksi.`nomember` not in (
+		SELECT
+			idcustomer
+		FROM
+			master.`jenis_trxonline`
+		WHERE
+			idcustomer > 10)*/
+		AND `storage_transaksi`.`outlet` in ({{snippet: Outlet filter franchise}})
+	union
+	select
+    		storage_transaksi.id,
+    		0 as kodedb_id,
+		storage_transaksi.outlet,
+		storage_transaksi.tgl_trx,
+		storage_transaksi.no_trx,
+		storage_transaksi.namapemesan,
+		storage_transaksi.tlptujuan,
+		storage_transaksi.bruto,
+		storage_transaksi.diskon_rupiah,
+		storage_transaksi.netto,
+		storage_transaksi.remark,
+		outlet.kode,
+		outlet.panjang,
+		tempo.customer.nama,
+		tempo.`customer`.`alamat`,
+		tempo.`customer`.`phone`,
+		tempo.`customer`.`customer_gender`,
+		tempo.`customer`.`customer_dob`,
+		{{snippet: Channel Dictionary}}
+	from
+		tempo.storage_transaksi
+	JOIN dci.outlet on storage_transaksi.outlet = outlet.kode
+	LEFT JOIN tempo.`customer` ON
+		`storage_transaksi`.`nomember_app` = tempo.`customer`.`nomember`
+		AND `storage_transaksi`.`nomember` = tempo.`customer`.`id`
+	where
+		storage_transaksi.spoil = 0 AND storage_transaksi.compliment = 0
+		AND
+		(`storage_transaksi`.`outlet` != 'XC'
+			OR `storage_transaksi`.`outlet` != 'XT' OR `storage_transaksi`.`outlet` != 'XX')
+		[[AND {{periode}}]]
+		and jenistrx in ('1','4','8')
+		and storage_transaksi.`status` not in ('0', '')
+	/*	and storage_transaksi.`nomember` not in (
+		SELECT
+			idcustomer
+		FROM
+			master.`jenis_trxonline`
+		WHERE
+			idcustomer > 10)*/
+		AND `storage_transaksi`.`outlet` in (
+		{{snippet: Outlet filter franchise}})
+	union
+	select
+    		storage_transaksi.id,
+    		0 as kodedb_id,
+		storage_transaksi.outlet,
+		storage_transaksi.tgl_trx,
+		storage_transaksi.no_trx,
+		storage_transaksi.namapemesan,
+		storage_transaksi.tlptujuan,
+		storage_transaksi.bruto,
+		storage_transaksi.diskon_rupiah,
+		storage_transaksi.netto,
+		storage_transaksi.remark,
+		outlet.kode,
+		outlet.panjang,
+		tempo.customer.nama,
+		tempo.`customer`.`alamat`,
+		tempo.`customer`.`phone`,
+		tempo.`customer`.`customer_gender`,
+		tempo.`customer`.`customer_dob`,
+		{{snippet: Channel Dictionary}}
+	from
+		sistemsurabaya.storage_transaksi
+	JOIN dci.outlet on storage_transaksi.outlet = outlet.kode
+	LEFT JOIN tempo.`customer` ON
+		`storage_transaksi`.`nomember_app` = tempo.`customer`.`nomember`
+		AND `storage_transaksi`.`nomember` = tempo.`customer`.`id`
+	where
+		storage_transaksi.spoil = 0 AND storage_transaksi.compliment = 0
+		AND
+		(`storage_transaksi`.`outlet` != 'XC'
+			OR `storage_transaksi`.`outlet` != 'XT' OR `storage_transaksi`.`outlet` != 'XX')
+		[[AND {{periode}}]]
+		and jenistrx in ('1','4','8')
+		and storage_transaksi.`status` not in ('0', '')
+	/*	and storage_transaksi.`nomember` not in (
+		SELECT
+			idcustomer
+		FROM
+			master.`jenis_trxonline`
+		WHERE
+			idcustomer > 10)*/
+		AND `storage_transaksi`.`outlet` in ({{snippet: Outlet filter franchise}})
+	) as src
+	left join dci.storage_transaksi_channel stc on src.id = stc.storage_transaksi_id AND src.kodedb_id = stc.kodedb_id
+) as storage_transaksi
+WHERE 1 = 1
+[[AND {{outlet}}]]
+[[AND {{channel}}]]
+group by outlet, channel, tgl_trx
---
+++ metabase/sales data franchise only/piechart.bak.sql
@@ -0,0 +1,168 @@
+SELECT
+  sum(bruto) AS "Total Gross Sales",
+  sum(diskon_rupiah) AS "Total Discount",
+  sum(netto) AS "Total Nett Sales",
+  count(storage_transaksi.no_trx) AS "Number of Orders",
+  channel AS "Channel"
+FROM
+  (
+    SELECT
+      tgl_trx,
+      outlet,
+      bruto,
+      diskon_rupiah,
+      netto,
+      no_trx,
+      CASE
+        WHEN coalesce(stc.channel, src.channel) IN ('Web', 'App') THEN 'Web + App'
+        WHEN coalesce(stc.channel, src.channel) IN ('GOFOOD', 'GRABFOOD', 'SHOPEE FOOD')
+        AND src.channel != 'Walk In' THEN 'Food Delivery'
+        WHEN coalesce(stc.channel, src.channel) IN (
+          'ECOMMERCE - Tokopedia',
+          'ECOMMERCE - SHOPEE MALL',
+          'Tik Tok'
+        ) THEN 'E-Commerce'
+        WHEN coalesce(src.channel = 'Walk In') THEN 'Walk In'
+        ELSE coalesce(src.channel, src.channel)
+      END AS channel
+    FROM
+      (
+        SELECT
+          storage_transaksi.id,
+          6 AS kodedb_id,
+          storage_transaksi.outlet,
+          storage_transaksi.tgl_trx,
+          storage_transaksi.no_trx,
+          storage_transaksi.namapemesan,
+          storage_transaksi.tlptujuan,
+          storage_transaksi.bruto,
+          storage_transaksi.diskon_rupiah,
+          storage_transaksi.netto,
+          storage_transaksi.remark,
+          outlet.kode,
+          outlet.panjang,
+          tempo.customer.nama,
+          tempo.`customer`.`alamat`,
+          tempo.`customer`.`phone`,
+          tempo.`customer`.`customer_gender`,
+          tempo.`customer`.`customer_dob`,
+          { { snippet: Channel Dictionary } }
+        FROM
+          dci.storage_transaksi
+          JOIN dci.outlet ON storage_transaksi.outlet = outlet.kode
+          LEFT JOIN tempo.`customer` ON `storage_transaksi`.`nomember_app` = tempo.`customer`.`nomember`
+          AND `storage_transaksi`.`nomember` = tempo.`customer`.`id`
+        WHERE
+          storage_transaksi.spoil = 0
+          AND storage_transaksi.compliment = 0
+          AND (
+            `storage_transaksi`.`outlet` != 'XC'
+            OR `storage_transaksi`.`outlet` != 'XT'
+            OR `storage_transaksi`.`outlet` != 'XX'
+          ) [[AND {{periode}}]]
+          AND jenistrx IN ('1', '4', '8')
+          AND storage_transaksi.`status` not in('0', '')
+          /*	and storage_transaksi.`nomember` not in (
+          SELECT
+          idcustomer
+          FROM
+          master.`jenis_trxonline`
+          WHERE
+          idcustomer > 10)*/
+          AND `storage_transaksi`.`outlet` IN ({{snippet: Outlet filter franchise}})
+        UNION
+        SELECT
+          storage_transaksi.id,
+          0 AS kodedb_id,
+          storage_transaksi.outlet,
+          storage_transaksi.tgl_trx,
+          storage_transaksi.no_trx,
+          storage_transaksi.namapemesan,
+          storage_transaksi.tlptujuan,
+          storage_transaksi.bruto,
+          storage_transaksi.diskon_rupiah,
+          storage_transaksi.netto,
+          storage_transaksi.remark,
+          outlet.kode,
+          outlet.panjang,
+          tempo.customer.nama,
+          tempo.`customer`.`alamat`,
+          tempo.`customer`.`phone`,
+          tempo.`customer`.`customer_gender`,
+          tempo.`customer`.`customer_dob`,
+          {{snippet: Channel Dictionary}}
+        FROM
+          tempo.storage_transaksi
+          JOIN dci.outlet ON storage_transaksi.outlet = outlet.kode
+          LEFT JOIN tempo.`customer` ON `storage_transaksi`.`nomember_app` = tempo.`customer`.`nomember`
+          AND `storage_transaksi`.`nomember` = tempo.`customer`.`id`
+        WHERE
+          storage_transaksi.spoil = 0
+          AND storage_transaksi.compliment = 0
+          AND (
+            `storage_transaksi`.`outlet` != 'XC'
+            OR `storage_transaksi`.`outlet` != 'XT'
+            OR `storage_transaksi`.`outlet` != 'XX'
+          ) [[AND {{periode}}]]
+          AND jenistrx IN ('1', '4', '8')
+          AND storage_transaksi.`status` not in('0', '')
+          /*	and storage_transaksi.`nomember` not in (
+          SELECT
+          idcustomer
+          FROM
+          master.`jenis_trxonline`
+          WHERE
+          idcustomer > 10)*/
+          AND `storage_transaksi`.`outlet` IN ({{snippet: Outlet filter franchise}})
+        UNION
+        SELECT
+          storage_transaksi.id,
+          0 AS kodedb_id,
+          storage_transaksi.outlet,
+          storage_transaksi.tgl_trx,
+          storage_transaksi.no_trx,
+          storage_transaksi.namapemesan,
+          storage_transaksi.tlptujuan,
+          storage_transaksi.bruto,
+          storage_transaksi.diskon_rupiah,
+          storage_transaksi.netto,
+          storage_transaksi.remark,
+          outlet.kode,
+          outlet.panjang,
+          tempo.customer.nama,
+          tempo.`customer`.`alamat`,
+          tempo.`customer`.`phone`,
+          tempo.`customer`.`customer_gender`,
+          tempo.`customer`.`customer_dob`,
+          {{snippet: Channel Dictionary}}
+        FROM
+          sistemsurabaya.storage_transaksi
+          JOIN dci.outlet ON storage_transaksi.outlet = outlet.kode
+          LEFT JOIN tempo.`customer` ON `storage_transaksi`.`nomember_app` = tempo.`customer`.`nomember`
+          AND `storage_transaksi`.`nomember` = tempo.`customer`.`id`
+        WHERE
+          storage_transaksi.spoil = 0
+          AND storage_transaksi.compliment = 0
+          AND (
+            `storage_transaksi`.`outlet` != 'XC'
+            OR `storage_transaksi`.`outlet` != 'XT'
+            OR `storage_transaksi`.`outlet` != 'XX'
+          ) [[AND {{periode}}]]
+          AND jenistrx IN ('1', '4', '8')
+          AND storage_transaksi.`status` not in('0', '')
+          /*	and storage_transaksi.`nomember` not in (
+          SELECT
+          idcustomer
+          FROM
+          master.`jenis_trxonline`
+          WHERE
+          idcustomer > 10)*/
+          AND `storage_transaksi`.`outlet` IN ({{snippet: Outlet filter franchise}})
+      ) AS src
+      LEFT JOIN dci.storage_transaksi_channel stc ON src.id = stc.storage_transaksi_id
+      AND src.kodedb_id = stc.kodedb_id
+  ) AS storage_transaksi
+WHERE
+  1 = 1 [[AND {{outlet}}]] [[AND {{channel}}]]
+GROUP BY
+  channel
