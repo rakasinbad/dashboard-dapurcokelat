d013d0974f28c43aea1d05b0b2dc7e194220a5cc
Reza Raka Nugraha <rezaraka00@gmail.com>
2026-04-15 17:31:44 +0700

    update

--- FIx gross sales tidak sama.sql
+++ FIx gross sales tidak sama.sql
@@ -66,7 +66,7 @@ SELECT
 	st.no_trx,
 	st.bruto,
 	sum(std.harga * std.qty) as total_harga,
-	x
+	bruto - sum(std.harga * std.qty) as perbandingan
 FROM
 	dci.storage_transaksi st
 	join dci.storage_transaksi_detail std on st.no_trx = std.no_trx
