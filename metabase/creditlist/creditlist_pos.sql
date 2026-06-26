7ed1ec0a98e4c118077a507469e72d1a8397e382
Reza Raka Nugraha <rezaraka00@gmail.com>
2026-06-26 11:15:31 +0700

    update query creditlist

--- metabase/creditlist/creditlist_pos.sql
+++ metabase/creditlist/creditlist_pos.sql
@@ -1,26 +1,31 @@
-SELECT
+select
   *
 FROM
-  creditlist
-WHERE
-  outlet in ('AD', 'PT', 'PM')
-  and (
-    (
-      tglbuku BETWEEN '2026-06-25'
-      AND '2026-06-25'
-      AND status != '0'
-    )
-    OR (
-      tglbuku < '2026-06-25'
-      AND STATUS = '1'
-    )
-    OR (
-      tglbuku < '2026-06-25'
-      AND STATUS = '2'
-      AND paiddate >= '2026-06-25'
-    )
-  )
-ORDER BY
-  jenis asc,
-  idcustomer ASC,
-  trxdate ASC
+  (
+    SELECT
+      *
+    FROM
+      dci.creditlist
+    WHERE
+      outlet in ('AD', 'PT', 'PM')
+      and (
+        (
+          tglbuku BETWEEN '2026-06-24'
+          AND '2026-06-25'
+          AND status != '0'
+        )
+        OR (
+          tglbuku < '2026-06-24'
+          AND STATUS = '1'
+        )
+        OR (
+          tglbuku < '2026-06-24'
+          AND STATUS = '2'
+          AND paiddate >= '2026-06-24'
+        )
+      )
+    ORDER BY
+      jenis asc,
+      idcustomer ASC,
+      trxdate ASC
+  ) as data
