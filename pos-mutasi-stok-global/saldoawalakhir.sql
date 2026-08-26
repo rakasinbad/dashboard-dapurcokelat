ddb826b586dd7823c29970ef0cf09f1d900dac65
Reza Raka Nugraha <rezaraka00@gmail.com>
2026-08-26 15:40:25 +0700

    add saldo awal akshir sql

--- pos-mutasi-stok-global/saldoawalakhir.sql
+++ pos-mutasi-stok-global/saldoawalakhir.sql
@@ -6,7 +6,8 @@ WITH prev_month AS (
   FROM dci.stok_card
   WHERE kode = '50010001'
     AND owner IN ('AD', 'AS', 'AP', 'AG', 'BL', 'BO', 'BK', 'CK', 'CI', 'ATMK', 'BB', 'BE', 'BN', 'BR', 'BA', 'CH', 'BU', 'CD', 'CP', 'DY', 'DA', 'DU', 'DK', 'DG', 'GM', 'GRR', 'GN', 'GW', 'JB', 'JU', 'JW', 'JG', 'KUR', 'KD', 'KT', 'KP', 'KC', 'MD', 'MN', 'MJ', 'MTH', 'MC', 'PG', 'PE', 'PL', 'PDR', 'PRT', 'PS', 'PU', 'PT', 'PN', 'PM', 'RM', 'RN', 'RK', 'SB', 'SC', 'SD', 'SK', 'SRT', 'TM', 'DT', 'TK', 'TL', 'TG', 'TR', 'TN', 'TD', 'VB', 'WY', 'BI', 'GS', 'GY', 'GO', 'GF', 'GV', 'GR', 'HI', 'JS', 'JGJ', 'KW', 'KG', 'LB', 'ML', 'ME', 'MT', 'MG', 'MS', 'OT', 'B2B', 'B2BS', 'DB', 'SY', 'SMG', 'ST', 'TP', 'TU', 'TB', 'TS', 'WTC')
-    AND created BETWEEN '2025-12-01' AND '2025-12-31 23:59:59'
+    AND created BETWEEN '2025-12-01 00:00:00' AND '2025-12-31 23:59:59'
+    AND jenis != 0
 ),
 -- Current month's latest balance for all owners
 curr_month AS (
@@ -17,10 +18,10 @@ curr_month AS (
   FROM dci.stok_card
   WHERE kode = '50010001'
     AND owner IN ('AD', 'AS', 'AP', 'AG', 'BL', 'BO', 'BK', 'CK', 'CI', 'ATMK', 'BB', 'BE', 'BN', 'BR', 'BA', 'CH', 'BU', 'CD', 'CP', 'DY', 'DA', 'DU', 'DK', 'DG', 'GM', 'GRR', 'GN', 'GW', 'JB', 'JU', 'JW', 'JG', 'KUR', 'KD', 'KT', 'KP', 'KC', 'MD', 'MN', 'MJ', 'MTH', 'MC', 'PG', 'PE', 'PL', 'PDR', 'PRT', 'PS', 'PU', 'PT', 'PN', 'PM', 'RM', 'RN', 'RK', 'SB', 'SC', 'SD', 'SK', 'SRT', 'TM', 'DT', 'TK', 'TL', 'TG', 'TR', 'TN', 'TD', 'VB', 'WY', 'BI', 'GS', 'GY', 'GO', 'GF', 'GV', 'GR', 'HI', 'JS', 'JGJ', 'KW', 'KG', 'LB', 'ML', 'ME', 'MT', 'MG', 'MS', 'OT', 'B2B', 'B2BS', 'DB', 'SY', 'SMG', 'ST', 'TP', 'TU', 'TB', 'TS', 'WTC')
-    AND created BETWEEN '2026-01-01' AND '2026-01-31 23:59:59'
+    AND created BETWEEN '2026-01-01 00:00:00' AND '2026-01-31 23:59:59'
     AND jenis != '0'
 )
-SELECT *
+SELECT SUM(start_balance), sum(latest_balance)
 FROM (
   SELECT curr.owner,
          COALESCE (prev.start_balance,
