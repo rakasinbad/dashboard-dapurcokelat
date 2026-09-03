eea537255af247b0a6e2f3287d583c47a9d830de
Reza Raka Nugraha <rezaraka00@gmail.com>
2026-09-03 15:33:27 +0700

    add query karyawan

---
+++ karyawan/bulk query insert question_depts.sql
@@ -0,0 +1,33 @@
+INSERT INTO
+  question_depts (
+    question_id,
+    dept,
+    sub_dept,
+    dept_declare,
+    jabatan
+  )
+VALUES
+  -- LEADER
+  (319, 'R&D', 'R&D', NULL, 'LEADER'),
+  (320, 'R&D', 'R&D', NULL, 'LEADER'),
+  (321, 'R&D', 'R&D', NULL, 'LEADER'),
+  (322, 'R&D', 'R&D', NULL, 'LEADER'),
+  (323, 'R&D', 'R&D', NULL, 'LEADER'),
+  -- ASSISTANT_SUPERVISOR
+  (319, 'R&D', 'R&D', NULL, 'ASSISTANT_SUPERVISOR'),
+  (320, 'R&D', 'R&D', NULL, 'ASSISTANT_SUPERVISOR'),
+  (321, 'R&D', 'R&D', NULL, 'ASSISTANT_SUPERVISOR'),
+  (322, 'R&D', 'R&D', NULL, 'ASSISTANT_SUPERVISOR'),
+  (323, 'R&D', 'R&D', NULL, 'ASSISTANT_SUPERVISOR'),
+  -- SUPERVISOR
+  (319, 'R&D', 'R&D', NULL, 'SUPERVISOR'),
+  (320, 'R&D', 'R&D', NULL, 'SUPERVISOR'),
+  (321, 'R&D', 'R&D', NULL, 'SUPERVISOR'),
+  (322, 'R&D', 'R&D', NULL, 'SUPERVISOR'),
+  (323, 'R&D', 'R&D', NULL, 'SUPERVISOR'),
+  -- SENIOR_SUPERVISOR
+  (319, 'R&D', 'R&D', NULL, 'SENIOR_SUPERVISOR'),
+  (320, 'R&D', 'R&D', NULL, 'SENIOR_SUPERVISOR'),
+  (321, 'R&D', 'R&D', NULL, 'SENIOR_SUPERVISOR'),
+  (322, 'R&D', 'R&D', NULL, 'SENIOR_SUPERVISOR'),
+  (323, 'R&D', 'R&D', NULL, 'SENIOR_SUPERVISOR');
